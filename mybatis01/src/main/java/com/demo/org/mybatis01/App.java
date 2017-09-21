caa813705e268741a99df36d99e045cfcf260bd7
644262163 <2468001771@qq.com>
2017-09-21 11:08:18 +0800

    mybatis demo

---
+++ mybatis01/src/main/java/com/demo/org/mybatis01/App.java
@@ -0,0 +1,57 @@
+package com.demo.org.mybatis01;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.List;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import com.demo.org.bean.Account;
+import com.demo.org.bean.Role;
+import com.demo.org.bean.UserLog;
+import com.demo.org.dao.ILog;
+import com.demo.org.dao.IRole;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws IOException
+    {
+        Reader reader = Resources.getResourceAsReader("config/config.xml");
+        SqlSessionFactory ssf = new SqlSessionFactoryBuilder().build(reader);
+        
+        SqlSession session = ssf.openSession();
+        //第一种映射方式 通过字符串描述域名与ID查找映射
+        List<Account> accountList = session.selectList("test.getAll");
+        for(Account acc : accountList) {
+        	System.out.println(acc.getUsername());
+        }
+        //第二种映射方式，通过接口映射
+        IRole roleDao = session.getMapper(IRole.class);
+        List<Role> roleList = roleDao.getAll();
+        for(Role role : roleList) {
+        	System.out.println(role.getRolename());
+        }
+        //第三种映射方式，通过注解
+        session.getConfiguration().addMapper(ILog.class);
+        ILog logDao = session.getMapper(ILog.class);
+        List<UserLog> logList = logDao.getAll();
+        for(UserLog ul : logList) {
+        	System.out.println(ul.getDescription());
+        }
+        
+        Account account = new Account();
+        account.setUsername("testname");
+        account.setPassword("testpassword");
+        session.insert("test.addAcc",account);
+        
+        session.commit();
+    }
+}
--- springdemo01/src/main/java/com/demo/org/springdemo01/App.java
+++ springdemo01/src/main/java/com/demo/org/springdemo01/App.java
@@ -17,7 +17,7 @@ public class App
     {
         ApplicationContext ac = new ClassPathXmlApplicationContext("config/service.xml");
         
-        /*BeanClass01 bc01 = ac.getBean("bc01",BeanClass01.class);
+        BeanClass01 bc01 = ac.getBean("bc01",BeanClass01.class);
         bc01.setMsg("This is bc01");
         bc01.showMsg();
         
@@ -27,14 +27,14 @@ public class App
         
         System.out.println("--------------第二次调用-------------");
         bc01.showMsg();
-        System.out.println(bc01 == bc0102);*/
+        System.out.println(bc01 == bc0102);
         
-        /*BeanClass01 bcf01 = ac.getBean("bcf01",BeanClass01.class);
+        BeanClass01 bcf01 = ac.getBean("bcf01",BeanClass01.class);
         bcf01.setMsg("This is bcf01");
-        bcf01.showMsg();*/
+        bcf01.showMsg();
         
-        /*BeanClass02 bc02 = ac.getBean("bc02",BeanClass02.class);
-        bc02.showInfo();*/
+        BeanClass02 bc02 = ac.getBean("bc02",BeanClass02.class);
+        bc02.showInfo();
         
         BeanClass03 bc03 = ac.getBean("bc03",BeanClass03.class);
         bc03.show();
